d661f9c201e811a657cdf0f3da997736018f1059
Rizky A <44380444+Tsucie@users.noreply.github.com>
2022-12-19 16:41:28 +0700

    Add chat button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -225,6 +225,15 @@
 		{{-- Include customer page footer --}}
 		@include('layouts.footers.csmainfooter')
 	</main>
+  <div class="sticky-chat-button">
+  <!-- telegram chat button  start -->
+  <a class="telegram-chat-button" href="https://t.me/admsilungkang_bot" title="Ingin bertanya sesuatu?" target="_blank">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32">
+      <path fill="#fff" d="M29.919 6.163l-4.225 19.925c-0.319 1.406-1.15 1.756-2.331 1.094l-6.438-4.744-3.106 2.988c-0.344 0.344-0.631 0.631-1.294 0.631l0.463-6.556 11.931-10.781c0.519-0.462-0.113-0.719-0.806-0.256l-14.75 9.288-6.35-1.988c-1.381-0.431-1.406-1.381 0.288-2.044l24.837-9.569c1.15-0.431 2.156 0.256 1.781 2.013z"/>
+    </svg>
+  </a>
+  <!-- telegram chat button end -->
+  </div>
 	<script>
 		function fungsiHiasan() {
 			document.getElementById("#produk-hiasan").click();
